e8ff49be33e404366c2466804bfc5ffe9c6fbd1b
atippaz <nasakun13201@gmail.com>
2024-02-26 22:47:39 +0700

    update question

--- TypeScript/qa.ts
+++ TypeScript/qa.ts
@@ -1,9 +1,30 @@
+interface IStudent{
+    id:string
+    name:string
+    birthDay:Date
+    major:Major
+}
+type Major='Sci'|'Engi'|'Art'|'Busi'
+
+const studentData:IStudent[] = [{
+    id:'1',
+    birthDay:new Date(),
+    major:'Art',
+    name:'atip'
+}]
+const newStudentData = []
+function initialQuestion(){
+    const art:Major ='Art'
+    const busi:Major ='Busi'
+    const engi:Major ='Engi'
+    const sci:Major ='Sci'
+    const data = [{name:'atp',major:sci}]
+    newStudentData.push(...data)
+}
 export const question1 = ()=>{
-    return  ''
 } 
 export const a = 1
 export default function(){
-    console.log('importing . . . ')
     return {
         a:1
     }
